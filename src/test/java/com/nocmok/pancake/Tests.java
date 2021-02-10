75523bcde29492a6aaee5e948ce51120659ec9b9
nocmok <nyusafonov@edu.hse.ru>
2021-02-10 20:54:00 +0300

    Tests class with tests static global information added

---
+++ src/test/java/com/nocmok/pancake/Tests.java
@@ -0,0 +1,12 @@
+package com.nocmok.pancake;
+
+import java.io.File;
+
+public class Tests {
+
+    public static final File resourcesDirectory = new File("src/test/res");
+
+    public static final File landsat8Directory = new File(resourcesDirectory, "landsat8");
+
+    public static final File deimos2Directory = new File(resourcesDirectory, "deimos2");
+}
