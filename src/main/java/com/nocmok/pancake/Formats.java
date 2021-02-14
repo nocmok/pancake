a42efcc2f09298bf9dec062ead88e4cfab575816
nocmok <nyusafonov@edu.hse.ru>
2021-02-14 23:19:03 +0300

    Formats enum added to handle some format specific logic

---
+++ src/main/java/com/nocmok/pancake/Formats.java
@@ -0,0 +1,98 @@
+package com.nocmok.pancake;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.nocmok.pancake.utils.PancakeOptions;
+
+import org.gdal.gdal.Driver;
+import org.gdal.gdal.gdal;
+
+public enum Formats {
+
+    GTiff("GTiff") {
+        @Override
+        public PancakeOptions toDriverOptions(PancakeOptions options) {
+            PancakeOptions driverOptions = new PancakeOptions();
+
+            driverOptions.put("INTERLEAVE", options.getStringOr(PancakeConstants.KEY_INTERLEAVE, "BAND"));
+            driverOptions.put("TILED", options.getBoolOr(PancakeConstants.KEY_TILED, true) ? "YES" : "NO");
+            driverOptions.put("BLOCKXSIZE", options.getIntOr(PancakeConstants.KEY_BLOCKXSIZE, 256));
+            driverOptions.put("BLOCKYSIZE", options.getIntOr(PancakeConstants.KEY_BLOCKYSIZE, 256));
+            driverOptions.put("COMPRESS", options.getStringOr(PancakeConstants.KEY_COMPRESSION, "NONE"));
+            driverOptions.put("NUM_THREADS", options.getIntOr(PancakeConstants.KEY_COMPRESSION_NUM_THREADS, 1));
+            driverOptions.put("PHOTOMETRIC", options.getStringOr(PancakeConstants.KEY_PHOTOMETRIC, "MINISBLACK"));
+            driverOptions.put("BIGTIFF", options.getStringOr(PancakeConstants.KEY_BIGTIFF, "NO"));
+
+            int compressionQuality, specificQuality;
+            Compression compression = Compression
+                    .valueOf(options.getStringOr(PancakeConstants.KEY_COMPRESSION, "NONE"));
+
+            switch (compression) {
+                case JPEG:
+                    compressionQuality = options.getIntOr(PancakeConstants.KEY_COMPRESSION_QUALITY, 75);
+                    specificQuality = compressionQuality;
+                    driverOptions.put("JPEG_QUALITY", specificQuality);
+                    break;
+                case Deflate:
+                    compressionQuality = options.getIntOr(PancakeConstants.KEY_COMPRESSION_QUALITY, 70);
+                    specificQuality = (int) (((double) compressionQuality) / 100 * 9);
+                    driverOptions.put("ZLEVEL", specificQuality);
+                    break;
+                case LERC_DEFLATE:
+                    compressionQuality = options.getIntOr(PancakeConstants.KEY_COMPRESSION_QUALITY, 50);
+                    specificQuality = (int) ((double) compressionQuality) / 100 * 9;
+                    driverOptions.put("ZLEVEL", specificQuality);
+                    break;
+                case ZSTD:
+                case LERC_ZSTD:
+                    compressionQuality = options.getIntOr(PancakeConstants.KEY_COMPRESSION_QUALITY, 42);
+                    specificQuality = (int) ((double) compressionQuality) / 100 * 9;
+                    driverOptions.put("ZSTD_LEVEL", specificQuality);
+                    break;
+                case WEBP:
+                    compressionQuality = options.getIntOr(PancakeConstants.KEY_COMPRESSION_QUALITY, 75);
+                    specificQuality = compressionQuality;
+                    driverOptions.put("WEBP_LEVEL", specificQuality);
+                    break;
+                default:
+                    break;
+            }
+            return driverOptions;
+        }
+    },
+
+    ;
+
+    private static final Map<String, Formats> nameFormatMapping = new HashMap<>();
+
+    static {
+        for (Formats format : Formats.values()) {
+            nameFormatMapping.put(format.driverName().toLowerCase(), format);
+        }
+    }
+
+    public static Formats byName(String name) {
+        return nameFormatMapping.get(name.toLowerCase());
+    }
+
+    private final String driverName;
+
+    private Formats(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String driverName() {
+        return driverName;
+    }
+
+    public Driver getDriver() {
+        return gdal.GetDriverByName(driverName);
+    }
+
+    /**
+     * Translate pancake specific image option notation, to gdal driver specific
+     * creation options notation
+     */
+    public abstract PancakeOptions toDriverOptions(PancakeOptions options);
+}
