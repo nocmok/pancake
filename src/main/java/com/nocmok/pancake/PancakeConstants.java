b13c4812db809e251403d98b65a1221c4abcbb37
nocmok <nyusafonov@edu.hse.ru>
2021-02-14 23:19:46 +0300

    added class with constants for internal usage

---
+++ src/main/java/com/nocmok/pancake/PancakeConstants.java
@@ -0,0 +1,22 @@
+package com.nocmok.pancake;
+
+public class PancakeConstants {
+    
+    public static final String KEY_COMPRESSION = "pnk_compress";
+
+    public static final String KEY_INTERLEAVE = "pnk_interleave";
+
+    public static final String KEY_TILED = "pnk_tiled";
+
+    public static final String KEY_BLOCKXSIZE = "pnk_blockxsize";
+
+    public static final String KEY_BLOCKYSIZE = "pnk_blockysize";
+
+    public static final String KEY_PHOTOMETRIC = "pnk_photometric";
+
+    public static final String KEY_BIGTIFF = "pnk_bigtiff";
+
+    public static final String KEY_COMPRESSION_NUM_THREADS = "pnk_compress_nthreads";
+
+    public static final String KEY_COMPRESSION_QUALITY = "pnk_compress_quality";
+}
