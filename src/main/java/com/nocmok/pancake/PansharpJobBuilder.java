be59dbe69978b80169ca08b6438c88cdc137d7f6
nocmok <nyusafonov@edu.hse.ru>
2021-02-14 23:21:43 +0300

    added builder class for PansharpJob

---
+++ src/main/java/com/nocmok/pancake/PansharpJobBuilder.java
@@ -0,0 +1,75 @@
+package com.nocmok.pancake;
+
+import java.io.File;
+import java.util.Map;
+
+import com.nocmok.pancake.fusor.Fusor;
+import com.nocmok.pancake.upsampler.Upsampler;
+import com.nocmok.pancake.utils.PancakeOptions;
+
+import org.gdal.gdal.Band;
+
+public class PansharpJobBuilder {
+
+    private PancakeOptions options;
+
+    private Upsampler resampler;
+
+    private Fusor fusor;
+
+    private Map<Spectrum, Band> mapping;
+
+    public PansharpJobBuilder() {
+        options = new PancakeOptions();
+    }
+
+    public PansharpJobBuilder withOutputFormat(Formats format) {
+        options.put(PansharpJob.JOB_TARGET_FORMAT, format.driverName());
+        return this;
+    }
+
+    public PansharpJobBuilder withResampler(Upsampler resampler) {
+        this.resampler = resampler;
+        return this;
+    }
+
+    public PansharpJobBuilder withFusor(Fusor fusor) {
+        this.fusor = fusor;
+        return this;
+    }
+
+    public PansharpJobBuilder withThreads(int numThreads) {
+        options.put(PansharpJob.JOB_NUM_THREADS, numThreads);
+        return this;
+    }
+
+    public PansharpJobBuilder withCompression(Compression compression) {
+        options.put(PansharpJob.JOB_COMPRESSION, compression.gdalIdentifier());
+        return this;
+    }
+
+    public PansharpJobBuilder withCompressionQuality(int quality) {
+        options.put(PansharpJob.JOB_COMPRESSION_QUALITY, quality);
+        return this;
+    }
+
+    public PansharpJobBuilder withOutputFile(File outputFile) {
+        options.put(PansharpJob.JOB_TARGET_PATH, outputFile.getAbsolutePath());
+        return this;
+    }
+
+    public PansharpJobBuilder withSource(Map<Spectrum, Band> source) {
+        this.mapping = source;
+        return this;
+    }
+
+    public PansharpJobBuilder with(String key, Object value) {
+        options.put(key, value);
+        return this;
+    }
+
+    public PansharpJob build() {
+        PansharpJob job = new PansharpJob(resampler, fusor, mapping, options);
+        return job;
+    }
+}
