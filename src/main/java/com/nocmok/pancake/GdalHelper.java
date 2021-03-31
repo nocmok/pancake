1c8abaf792bd0a5eb3b3a6f9a79f9a5c6e27060a
nocmok <nyusafonov@edu.hse.ru>
2021-03-31 23:21:07 +0300

    gdal util methods moved to GdalHelper class

---
+++ src/main/java/com/nocmok/pancake/GdalHelper.java
@@ -0,0 +1,156 @@
+package com.nocmok.pancake;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Vector;
+
+import org.gdal.gdal.Band;
+import org.gdal.gdal.Dataset;
+import org.gdal.gdal.Driver;
+import org.gdal.gdal.gdal;
+
+public class GdalHelper {
+
+    public static String pathTo(Dataset dataset) {
+        Vector<?> fileList = dataset.GetFileList();
+        return fileList != null ? fileList.firstElement().toString() : null;
+    }
+
+    public static List<Band> split(Dataset dataset) {
+        List<Band> bands = new ArrayList<>();
+        for (int nBand = 1; nBand <= dataset.getRasterCount(); ++nBand) {
+            bands.add(dataset.GetRasterBand(nBand));
+        }
+        return bands;
+    }
+
+    public static void copyBand(Band dst, Band src) {
+        if (dst.GetXSize() != src.getXSize() || dst.getYSize() != src.getYSize()) {
+            throw new UnsupportedOperationException("source band resolution mismatch destination band resolution");
+        }
+        if (dst.GetRasterDataType() == src.GetRasterDataType()) {
+            copyBandBlockwise(dst, src);
+        } else {
+            copyBandWithTypeConversion(dst, src);
+            throw new UnsupportedOperationException("copying bands with different sample data type not implemented");
+        }
+    }
+
+    private static void copyBandBlockwise(Band dst, Band src) {
+        int xBlocks = (src.getXSize() + src.GetBlockXSize() - 1) / src.GetBlockXSize();
+        int yBlocks = (src.getYSize() + src.GetBlockYSize() - 1) / src.GetBlockYSize();
+
+        int blockByteSize = src.GetBlockXSize() * src.GetBlockYSize() * gdal.GetDataTypeSize(src.GetRasterDataType())
+                / 8;
+        ByteBuffer buf = ByteBuffer.allocateDirect(blockByteSize);
+
+        for (int y = 0; y < yBlocks; ++y) {
+            for (int x = 0; x < xBlocks; ++x) {
+                src.ReadBlock_Direct(x, y, buf);
+                dst.WriteBlock_Direct(x, y, buf);
+            }
+        }
+    }
+
+    private static void copyBandWithTypeConversion(Band dst, Band src) {
+
+    }
+
+    public static Dataset bundleRgbBands(Band r, Band g, Band b, Driver driver, File dstFile, int bandsDataType,
+            List<String> options) {
+        List<Band> bands = List.of(r, g, b);
+        int xSize = r.getXSize();
+        int ySize = r.getYSize();
+        for (Band band : bands) {
+            if (band.getXSize() != xSize || band.getYSize() != ySize) {
+                throw new UnsupportedOperationException("bands should have equal resolution");
+            }
+        }
+        List<String> creationOptions = new ArrayList<>();
+        creationOptions.add("PHOTOMETRIC=rgb");
+        if (isTiled(r)) {
+            creationOptions.add("TILED=YES");
+        }
+        creationOptions.add("BLOCKXSIZE=" + r.GetBlockXSize());
+        creationOptions.add("BLOCKYSIZE=" + r.GetBlockYSize());
+        // Vector<String> creationOptions = new Vector<>(List.of("PHOTOMETRIC=rgb"));
+        if (options != null) {
+            creationOptions.addAll(options);
+        }
+        Dataset outDataset = driver.Create(dstFile.getAbsolutePath(), xSize, ySize, 3, bandsDataType,
+                new Vector<String>(creationOptions));
+
+        copyBand(outDataset.GetRasterBand(1), r);
+        copyBand(outDataset.GetRasterBand(2), g);
+        copyBand(outDataset.GetRasterBand(3), b);
+
+        return outDataset;
+    }
+
+    public static Dataset bundleRgbBands(Band r, Band g, Band b, Driver driver, File dstFile, int bandsDataType) {
+        return bundleRgbBands(r, g, b, driver, dstFile, bandsDataType, null);
+    }
+
+    public static boolean isTiled(Band band) {
+        return (band.GetBlockXSize() != band.getXSize());
+    }
+
+    public static Dataset bundleBandsToVRT(Collection<Band> bands, File dstFile, List<String> options) {
+        Band first = bands.iterator().next();
+        for (Band band : bands) {
+            if (first.GetRasterDataType() != band.GetRasterDataType()) {
+                throw new RuntimeException("all bands expected to have same datatype");
+            }
+        }
+        return bundleBandsToVRT(bands, dstFile, first.GetRasterDataType(), options);
+    }
+
+    public static Dataset bundleBandsToVRT(Collection<Band> bands, File dstFile, int dataType, List<String> options) {
+        int xSize = bands.iterator().next().getXSize();
+        int ySize = bands.iterator().next().getYSize();
+        String[] optionsArr = (options == null) ? (null) : (options.toArray(new String[0]));
+        Driver driver = gdal.GetDriverByName("VRT");
+        driver.Register();
+        Dataset dataset = driver.Create(dstFile.getAbsolutePath(), xSize, ySize, 0, dataType, optionsArr);
+
+        String sourceXmlPattern = "<SimpleSource>" + "<SourceFilename relativeToVRT=\"0\">%s</SourceFilename>"
+                + "<SourceBand>%d</SourceBand>"
+                + "<SourceProperties RasterXSize=\"%d\" RasterYSize=\"%d\" DataType=\"%s\" BlockXSize=\"%d\" BlockYSize=\"%d\"/>"
+                + "</SimpleSource>";
+
+        for (Band band : bands) {
+            dataset.AddBand(dataType);
+            Band vrtBand = dataset.GetRasterBand(dataset.getRasterCount());
+            String sourceSpecXml = String.format(sourceXmlPattern, pathTo(band.GetDataset()), band.GetBand(),
+                    band.getXSize(), band.getYSize(), gdal.GetDataTypeName(band.GetRasterDataType()),
+                    band.GetBlockXSize(), band.GetBlockYSize());
+            vrtBand.SetMetadataItem("source_" + (vrtBand.GetBand() - 1), sourceSpecXml, "new_vrt_sources");
+        }
+
+        return dataset;
+    }
+
+    public static Dataset copyDataset(File dstFile, Dataset srcDataset) {
+        Driver driver = srcDataset.GetDriver();
+        return driver.CreateCopy(dstFile.getAbsolutePath(), srcDataset, (String[]) null);
+    }
+
+    /**
+     * 
+     * @param ds
+     * @return 0 - min, 1 - max
+     */
+    public static double[] computeMinMax(Dataset ds) {
+        double[] minMax = new double[2];
+        for (int i = 1; i <= ds.getRasterCount(); ++i) {
+            double[] currMinMax = new double[2];
+            ds.GetRasterBand(i).ComputeRasterMinMax(currMinMax);
+            minMax[0] = Double.min(minMax[0], currMinMax[0]);
+            minMax[1] = Double.max(minMax[1], currMinMax[1]);
+        }
+        return minMax;
+    }
+}
--- src/main/java/com/nocmok/pancake/Pancake.java
+++ src/main/java/com/nocmok/pancake/Pancake.java
@@ -2,18 +2,12 @@ package com.nocmok.pancake;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Vector;
 
 import com.nocmok.pancake.utils.PancakeIOException;
 
-import org.gdal.gdal.Band;
-import org.gdal.gdal.Dataset;
-import org.gdal.gdal.Driver;
 import org.gdal.gdal.gdal;
+import org.opencv.core.Core;
 
 public class Pancake {
 
@@ -41,8 +35,15 @@ public class Pancake {
     /** wrapper for gdalconst.GDT_Float64 */
     public static final int TYPE_FLOAT_64 = 7;
 
+    public static final int TYPE_SBYTE = 100;
+
     private Pancake() {
 
+    }
+
+    public static void load() {
+        gdal.AllRegister();
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
     public static File createTempFile() {
@@ -55,50 +56,37 @@ public class Pancake {
         }
     }
 
-    public static String pathTo(Dataset dataset) {
-        Vector<?> fileList = dataset.GetFileList();
-        return fileList != null ? fileList.firstElement().toString() : null;
-    }
-
-    public static List<Band> getBands(Dataset dataset) {
-        List<Band> bands = new ArrayList<>();
-        for (int nBand = 1; nBand <= dataset.getRasterCount(); ++nBand) {
-            bands.add(dataset.GetRasterBand(nBand));
-        }
-        return bands;
-    }
-
     public static boolean isIntegerDatatype(int datatype) {
         switch (datatype) {
-            case Pancake.TYPE_BYTE:
-            case Pancake.TYPE_UNKNOWN:
-            case Pancake.TYPE_INT_16:
-            case Pancake.TYPE_UINT_16:
-            case Pancake.TYPE_INT_32:
-            case Pancake.TYPE_UINT_32:
-                return true;
-            case Pancake.TYPE_FLOAT_32:
-            case Pancake.TYPE_FLOAT_64:
-                return false;
-            default:
-                throw new UnsupportedOperationException("unsupported data type " + datatype);
+        case Pancake.TYPE_BYTE:
+        case Pancake.TYPE_UNKNOWN:
+        case Pancake.TYPE_INT_16:
+        case Pancake.TYPE_UINT_16:
+        case Pancake.TYPE_INT_32:
+        case Pancake.TYPE_UINT_32:
+            return true;
+        case Pancake.TYPE_FLOAT_32:
+        case Pancake.TYPE_FLOAT_64:
+            return false;
+        default:
+            throw new UnsupportedOperationException("unsupported data type " + datatype);
         }
     }
 
     public static boolean isUnsignedInt(int datatype) {
         switch (datatype) {
-            case Pancake.TYPE_BYTE:
-            case Pancake.TYPE_UNKNOWN:
-            case Pancake.TYPE_UINT_16:
-            case Pancake.TYPE_UINT_32:
-                return true;
-            case Pancake.TYPE_INT_32:
-            case Pancake.TYPE_INT_16:
-            case Pancake.TYPE_FLOAT_32:
-            case Pancake.TYPE_FLOAT_64:
-                return false;
-            default:
-                throw new UnsupportedOperationException("unsupported data type " + datatype);
+        case Pancake.TYPE_BYTE:
+        case Pancake.TYPE_UNKNOWN:
+        case Pancake.TYPE_UINT_16:
+        case Pancake.TYPE_UINT_32:
+            return true;
+        case Pancake.TYPE_INT_32:
+        case Pancake.TYPE_INT_16:
+        case Pancake.TYPE_FLOAT_32:
+        case Pancake.TYPE_FLOAT_64:
+            return false;
+        default:
+            throw new UnsupportedOperationException("unsupported data type " + datatype);
         }
     }
 
@@ -108,45 +96,45 @@ public class Pancake {
 
     public static double getDatatypeMax(int datatype) {
         switch (datatype) {
-            case Pancake.TYPE_BYTE:
-            case Pancake.TYPE_UNKNOWN:
-                return 0xff;
-            case Pancake.TYPE_INT_16:
-                return 0x7fff;
-            case Pancake.TYPE_UINT_16:
-                return 0xffff;
-            case Pancake.TYPE_INT_32:
-                return 0x7fffffff;
-            case Pancake.TYPE_UINT_32:
-                return 0xffffffffL;
-            case Pancake.TYPE_FLOAT_32:
-                return Float.MAX_VALUE;
-            case Pancake.TYPE_FLOAT_64:
-                return Double.MAX_VALUE;
-            default:
-                throw new UnsupportedOperationException("unknown data type " + datatype);
+        case Pancake.TYPE_BYTE:
+        case Pancake.TYPE_UNKNOWN:
+            return 0xff;
+        case Pancake.TYPE_INT_16:
+            return 0x7fff;
+        case Pancake.TYPE_UINT_16:
+            return 0xffff;
+        case Pancake.TYPE_INT_32:
+            return 0x7fffffff;
+        case Pancake.TYPE_UINT_32:
+            return 0xffffffffL;
+        case Pancake.TYPE_FLOAT_32:
+            return Float.MAX_VALUE;
+        case Pancake.TYPE_FLOAT_64:
+            return Double.MAX_VALUE;
+        default:
+            throw new UnsupportedOperationException("unknown data type " + datatype);
         }
     }
 
     public static double getDatatypeMin(int datatype) {
         switch (datatype) {
-            case Pancake.TYPE_BYTE:
-            case Pancake.TYPE_UNKNOWN:
-                return Byte.MIN_VALUE;
-            case Pancake.TYPE_INT_16:
-                return Short.MIN_VALUE;
-            case Pancake.TYPE_UINT_16:
-                return 0;
-            case Pancake.TYPE_INT_32:
-                return Integer.MIN_VALUE;
-            case Pancake.TYPE_UINT_32:
-                return 0;
-            case Pancake.TYPE_FLOAT_32:
-                return -Float.MAX_VALUE;
-            case Pancake.TYPE_FLOAT_64:
-                return -Double.MAX_VALUE;
-            default:
-                throw new UnsupportedOperationException("unknown data type " + datatype);
+        case Pancake.TYPE_BYTE:
+        case Pancake.TYPE_UNKNOWN:
+            return 0;
+        case Pancake.TYPE_INT_16:
+            return Short.MIN_VALUE;
+        case Pancake.TYPE_UINT_16:
+            return 0;
+        case Pancake.TYPE_INT_32:
+            return Integer.MIN_VALUE;
+        case Pancake.TYPE_UINT_32:
+            return 0;
+        case Pancake.TYPE_FLOAT_32:
+            return -Float.MAX_VALUE;
+        case Pancake.TYPE_FLOAT_64:
+            return -Double.MAX_VALUE;
+        default:
+            throw new UnsupportedOperationException("unknown data type " + datatype);
         }
     }
 
@@ -164,105 +152,7 @@ public class Pancake {
         return biggestDt;
     }
 
-    public static String getDatatypeName(int dtype){
+    public static String getDatatypeName(int dtype) {
         return gdal.GetDataTypeName(dtype);
-    }
-
-    public static void copyBand(Band dst, Band src) {
-        if (dst.GetXSize() != src.getXSize() || dst.getYSize() != src.getYSize()) {
-            throw new UnsupportedOperationException("source band resolution mismatch destination band resolution");
-        }
-        if (dst.GetRasterDataType() == src.GetRasterDataType()) {
-            copyBandBlockwise(dst, src);
-        } else {
-            copyBandWithTypeConversion(dst, src);
-            throw new UnsupportedOperationException("copying bands with different sample data type not implemented");
-        }
-    }
-
-    private static void copyBandBlockwise(Band dst, Band src) {
-        int xBlocks = (src.getXSize() + src.GetBlockXSize() - 1) / src.GetBlockXSize();
-        int yBlocks = (src.getYSize() + src.GetBlockYSize() - 1) / src.GetBlockYSize();
-
-        int blockByteSize = src.GetBlockXSize() * src.GetBlockYSize() * gdal.GetDataTypeSize(src.GetRasterDataType())
-                / 8;
-        ByteBuffer buf = ByteBuffer.allocateDirect(blockByteSize);
-
-        for (int y = 0; y < yBlocks; ++y) {
-            for (int x = 0; x < xBlocks; ++x) {
-                src.ReadBlock_Direct(x, y, buf);
-                dst.WriteBlock_Direct(x, y, buf);
-            }
-        }
-    }
-
-    private static void copyBandWithTypeConversion(Band dst, Band src) {
-
-    }
-
-    public static Dataset bundleRgbBands(Band r, Band g, Band b, Driver driver, File dstFile, int bandsDataType,
-            List<String> options) {
-        List<Band> bands = List.of(r, g, b);
-        int xSize = r.getXSize();
-        int ySize = r.getYSize();
-        for (Band band : bands) {
-            if (band.getXSize() != xSize || band.getYSize() != ySize) {
-                throw new UnsupportedOperationException("bands should have equal resolution");
-            }
-        }
-        List<String> creationOptions = new ArrayList<>();
-        creationOptions.add("PHOTOMETRIC=rgb");
-        if (isTiled(r)) {
-            creationOptions.add("TILED=YES");
-        }
-        creationOptions.add("BLOCKXSIZE=" + r.GetBlockXSize());
-        creationOptions.add("BLOCKYSIZE=" + r.GetBlockYSize());
-        // Vector<String> creationOptions = new Vector<>(List.of("PHOTOMETRIC=rgb"));
-        if (options != null) {
-            creationOptions.addAll(options);
-        }
-        Dataset outDataset = driver.Create(dstFile.getAbsolutePath(), xSize, ySize, 3, bandsDataType,
-                new Vector<String>(creationOptions));
-
-        copyBand(outDataset.GetRasterBand(1), r);
-        copyBand(outDataset.GetRasterBand(2), g);
-        copyBand(outDataset.GetRasterBand(3), b);
-
-        return outDataset;
-    }
-
-    public static Dataset bundleRgbBands(Band r, Band g, Band b, Driver driver, File dstFile, int bandsDataType) {
-        return bundleRgbBands(r, g, b, driver, dstFile, bandsDataType, null);
-    }
-
-    public static boolean isTiled(Band band) {
-        return (band.GetBlockXSize() != band.getXSize());
-    }
-
-    public static Dataset bundleBandsToVRT(Collection<Band> bands, File dstFile, int dataType, List<String> options) {
-        int xSize = bands.iterator().next().getXSize();
-        int ySize = bands.iterator().next().getYSize();
-        String[] optionsArr = (options == null) ? (null) : (options.toArray(new String[0]));
-        Driver driver = gdal.GetDriverByName("VRT");
-        driver.Register();
-        Dataset dataset = driver.Create(dstFile.getAbsolutePath(), xSize, ySize, 0, dataType, optionsArr);
-        String sourceXmlPattern = "<SimpleSource>" + "<SourceFilename relativeToVRT=\"0\">%s</SourceFilename>"
-                + "<SourceBand>%d</SourceBand>"
-                + "<SourceProperties RasterXSize=\"%d\" RasterYSize=\"%d\" DataType=\"%s\" BlockXSize=\"%d\" BlockYSize=\"%d\"/>"
-                + "</SimpleSource>";
-        for (Band band : bands) {
-            dataset.AddBand(dataType);
-            Band vrtBand = dataset.GetRasterBand(dataset.getRasterCount());
-            String sourceSpecXml = String.format(sourceXmlPattern, ((String) band.GetDataset().GetFileList().get(0)),
-                    band.GetBand(), band.getXSize(), band.getYSize(), gdal.GetDataTypeName(band.GetRasterDataType()),
-                    band.GetBlockXSize(), band.GetBlockYSize());
-            vrtBand.SetMetadataItem("source_" + (vrtBand.GetBand() - 1), sourceSpecXml, "new_vrt_sources");
-        }
-        return dataset;
-    }
-
-    public static Dataset copyDataset(File dstFile, Dataset srcDataset) {
-        Driver driver = srcDataset.GetDriver();
-        return driver.CreateCopy(dstFile.getAbsolutePath(), srcDataset, (String[]) null);
     }
 }
