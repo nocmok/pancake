4b5c55768293c3d480e2b72f4b9a5728bd3e136c
nocmok <nyusafonov@edu.hse.ru>
2021-04-01 22:26:42 +0300

    pojo class to store sizes

---
+++ src/main/java/com/nocmok/pancake/utils/Shape.java
@@ -0,0 +1,25 @@
+package com.nocmok.pancake.utils;
+
+public class Shape {
+
+    private int xsize;
+
+    private int ysize;
+
+    public Shape(int xsize, int ysize) {
+        this.xsize = xsize;
+        this.ysize = ysize;
+    }
+
+    public int xsize() {
+        return xsize;
+    }
+
+    public int ysize() {
+        return ysize;
+    }
+
+    public static Shape of(int xsize, int ysize) {
+        return new Shape(xsize, ysize);
+    }
+}
